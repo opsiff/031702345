ea5aab369eb3b668c19677f5a271a5df4661d8a2
opsiff <bujk@foxmail.com>
2019-09-28 19:29:12 +0800

    fix return json ensure

--- 031702345.py
+++ 031702345.py
@@ -18,8 +18,7 @@ def printTojson1(name,tel,address):
     ans['姓名']=name
     ans['手机']=tel
     ans['地址']=address
-    #jsonarray = json.dumps(ans, ensure_ascii=False)
-    jsonarray = json.dumps(ans, ensure_ascii)
+    jsonarray = json.dumps(ans, ensure_ascii=False)
     print(jsonarray)
     return
 
