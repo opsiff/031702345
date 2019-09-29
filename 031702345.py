32f7dfc17cd059f59de90be49c04b3ce5fe31275
opsiff <otgwt@outlook.com>
2019-09-29 19:53:45 +0800

    更新：减少了不必要的注释和代码

--- 031702345.py
+++ 031702345.py
@@ -10,8 +10,6 @@ import re
 #import jieba
 #jieba is included in cpca
 #chinese_province_city_area_mapper https://github.com/DQinYuan/chinese_province_city_area_mapper
-#刘六,福州市南屿镇五峰里1号福州旗山森林国家旅游区
-#王一,上海市浦东新区锦13101111111绣路1001号世纪公园
 
 # print to json
 def printTojson1(name,tel,address):
@@ -32,7 +30,6 @@ def threeAddress(rawaddress):
     return ans
 
 def fourthAddress(rawaddress):
-    #街道 镇 乡 地区 产业基地 开发区
     findss=[]
     for line in towns:
         sss=line[:-1]
@@ -44,34 +41,6 @@ def fourthAddress(rawaddress):
     findss.append(rawaddress)
     town.close()
     return findss
-'''''''''
-    addr4 = rawaddress.split('镇', 1)
-    if len(addr4) > 1:
-        addr4[0] += "镇"
-    else:
-        addr4 = rawaddress.split('街道', 1)
-        if len(addr4) > 1:
-            addr4[0] += "街道"
-        else:
-            addr4 = rawaddress.split('乡', 1)
-            if len(addr4) > 1:
-                addr4[0] += "乡"
-            else:
-                addr4 = rawaddress.split('地区', 1)
-                if len(addr4) > 1:
-                    addr4[0] += "地区"
-                else:
-                    addr4 = rawaddress.split('产业基地', 1)
-                    if len(addr4) > 1:
-                        addr4[0] += "产业基地"
-                    else:
-                        addr4 = rawaddress.split('开发区', 1)
-                        if len(addr4) > 1:
-                            addr4[0] += "开发区"
-                        else:
-                            addr4.insert(0, '')
-    return addr4
-'''''''''
 
 def fivethAddress(rawaddress):
     #路 街
@@ -113,9 +82,6 @@ def diffMode1(rawaddress):
         address[2] = ''
     address[0]=addressTransfr(address[0])
     addr4=fourthAddress(address[3])
-    #print(addr4)
-    #rawaddress=jieba.lcut(address[3])
-    #print(rawaddress)
     ans = [address[0], address[1], address[2], addr4[0],addr4[1]]
     return ans
 
@@ -130,11 +96,9 @@ def diffMode2(rawaddress):
 
 def diffMode3(rawaddress):
     my_key = '4217b885adb16e0541338722c26beded'
-
     parameters_1 = {'key': my_key, 'address': rawaddress}
     url_1 = 'https://restapi.amap.com/v3/geocode/geo?parameters'
     ret_1 = requests.get(url_1, parameters_1).json()
-
     location = ret_1['geocodes'][0]['location']
     parameters_2 = {'key': my_key, 'location': location}
     url_2 = 'https://restapi.amap.com/v3/geocode/regeo?parameters'
@@ -146,10 +110,8 @@ def diffMode3(rawaddress):
         addressthree[6]=rawaddress[1]
     return addressthree
 
-def main(rawaddress):
-    # rawInputFromConsole():
-    # runmode: default is 1
-    mode = 0
+def main(rawaddress):# rawInputFromConsole():
+    mode = 0# runmode: default is 1
     modestring = rawaddress[:1]
     if  modestring == "1":
         mode=1
@@ -185,10 +147,7 @@ def main(rawaddress):
         i = i + 1
     if numberfind == True:
         donothing = 1
-        #debug:print("success get phone number:")
-        #debug:print(number)
     else:
-        #debug:print("cannot get phone number")
         number = ""
     address = mixaddress[:thispos-(numberlength-1)]+mixaddress[thispos+1:]
     # remove . in the end
@@ -215,3 +174,33 @@ while 1:
 
     main(inputraw)
 town.close()
+
+'''''''''
+    #街道 镇 乡 地区 产业基地 开发区
+    addr4 = rawaddress.split('镇', 1)
+    if len(addr4) > 1:
+        addr4[0] += "镇"
+    else:
+        addr4 = rawaddress.split('街道', 1)
+        if len(addr4) > 1:
+            addr4[0] += "街道"
+        else:
+            addr4 = rawaddress.split('乡', 1)
+            if len(addr4) > 1:
+                addr4[0] += "乡"
+            else:
+                addr4 = rawaddress.split('地区', 1)
+                if len(addr4) > 1:
+                    addr4[0] += "地区"
+                else:
+                    addr4 = rawaddress.split('产业基地', 1)
+                    if len(addr4) > 1:
+                        addr4[0] += "产业基地"
+                    else:
+                        addr4 = rawaddress.split('开发区', 1)
+                        if len(addr4) > 1:
+                            addr4[0] += "开发区"
+                        else:
+                            addr4.insert(0, '')
+    return addr4
+'''''''''
