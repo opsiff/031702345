ecefd8c6528b97079b378055932c5eb02e409fe3
opsiff <otgwt@outlook.com>
2019-10-06 00:33:25 +0800

    fix a huge bug

--- 031702345.py
+++ 031702345.py
@@ -32,7 +32,7 @@ def printTojson1(name,tel,address):
     return
 
 def threeAddress(rawaddress):
-    address = numpy.array(cpca.transform([rawaddress], umap={}))[0]
+    address = numpy.array(cpca.transform([rawaddress]))[0]
     return [address[0],address[1],address[2],address[3]]
 
 def fourthAddress(rawaddress):
@@ -149,6 +149,10 @@ def main(rawaddress):# rawInputFromConsole():
     address = mixaddress[:thispos-(numberlength-1)]+mixaddress[thispos+1:]
     # remove . in the end
     address = address.split(".")[0]
+    addr = re.match(r".*(北京|天津|上海|重庆|北京市|天津市|上海市|重庆市|河北省|山西省|辽宁省|吉林省|黑龙江省|江苏省|浙江省|安徽省|福建省|江西省|山东省|河南省|湖北省|湖南省|广东省|海南省|四川省|贵州省|云南省|陕西省|甘肃省|青海省|台湾省|内蒙古自治区|广西壮族自治区|西藏自治区|宁夏回族自治区|新疆维吾尔自治区|香港特别行政区|澳门特别行政区)", address)
+    if addr == None:
+        address=address[:2]+'省'+address[2:]
+        print(address)
     if mode == 1 or mode == 0:
         address = diffMode1(address)
     elif mode == 2:
